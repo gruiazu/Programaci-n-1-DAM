872ee8389392288f4c6f6c699bb11bde8a7e5346
gruiazu <117372811+gruiazu@users.noreply.github.com>
2022-11-08 19:17:51 +0100

    Add files via upload

---
+++ Variables3.java
@@ -0,0 +1,15 @@
+package JAVA;
+
+public class Variables3 {
+    public static void main(String[] args) {
+    String Nombre;
+    String Dirección;
+    String Telefono;
+    Nombre= "Gonzalo";
+    Dirección= "Calle Valdepeñas 12";
+    Telefono= "684026848";
+    System.out.println(Nombre);
+    System.out.println(Dirección);
+    System.out.println(Telefono);
+    }    
+}
